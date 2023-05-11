4c2354969405833cd8b2e7aba3245732bb955b29
Kerem Ergür <ergur.kerem@gmail.com>
2023-05-11 21:10:09 +0200

    navbar

---
+++ src/components/layout/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li><RouterLink to="/dashboard">Dashboard</RouterLink></li>
+    <li><RouterLink to="/inbox">Inbox</RouterLink></li>
+    <li><RouterLink to="/important">Important</RouterLink></li>
+    <li><RouterLink to="/starred">Starred</RouterLink></li>
+    <li><RouterLink to="/sent">Sent</RouterLink></li>
+    <li><RouterLink to="/spam">Spam</RouterLink></li>
+    <li><RouterLink to="/trash">Trash</RouterLink></li>
+  </ul>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+ul {
+  justify-content: center;
+  padding: 0;
+}
+</style>
