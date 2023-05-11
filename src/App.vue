0d9d5dabd4d8e4c99533ec7b76c51a9b57b7c5f1
Kerem Ergür <ergur.kerem@gmail.com>
2023-05-11 21:11:26 +0200

    app template with new components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
+  <TheHeader />
+  <aside>
+    <Navigation />
+  </aside>
   <RouterView />
 </template>
 
-<script>
+<script setup>
+import TheHeader from "./components/layout/TheHeader.vue";
+import Navigation from "./components/layout/Navigation.vue";
 
 </script>
 
 <style>
-
+li {
+  list-style: none;
+}
+aside {
+  width: 10rem;
+  border: 0px solid red;
+  position: fixed;
+}
 </style>
