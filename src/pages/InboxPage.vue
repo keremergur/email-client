b11a40819548558f355d150c84710eed74605d32
Kerem Ergür <ergur.kerem@gmail.com>
2023-05-11 21:11:08 +0200

    inbox

---
+++ src/pages/InboxPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <ul>
+      <li v-for="email in emails" :key="email.id" >
+        <h4>{{ email.subject }}</h4>
+        <p>{{ email.content }}</p>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script setup>
+
+import {ref} from "vue";
+
+import mock from "./MOCK_DATA.js";
+const emails = ref(mock);
+
+</script>
+
+<style scoped>
+section {
+  border: 0px solid blue;
+  margin-left: 10rem;
+}
+</style>
