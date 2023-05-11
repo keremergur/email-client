3e74b6701a676dec4ff3fb2961f946ea85d3442c
Kerem Ergür <ergur.kerem@gmail.com>
2023-05-11 21:10:33 +0200

    small fixes

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import InboxPage from './pages/InboxPage.vue';
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/dashboard' },
         { path: '/dashboard', component: null },
         { path: '/auth', component: null },
-        { path: '/inbox', component: null },
+        { path: '/inbox', component: InboxPage },
         { path: '/important', component: null },
         { path: '/starred', component: null },
         { path: '/sent', component: null },
