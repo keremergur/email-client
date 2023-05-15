99f89df379c69efaa0400734f6c9e911f01506b2
Kerem Ergür <ergur.kerem@gmail.com>
2023-05-15 10:20:31 +0200

    Routing remade

--- src/router.js
+++ src/router.js
@@ -5,15 +5,15 @@ import InboxPage from './pages/InboxPage.vue';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', redirect: '/dashboard' },
-        { path: '/dashboard', component: null },
+        { path: '/', redirect: '/view' },
         { path: '/auth', component: null },
-        { path: '/inbox', component: InboxPage },
-        { path: '/important', component: null },
-        { path: '/starred', component: null },
-        { path: '/sent', component: null },
-        { path: '/trash', component: null },
-    ]
+        { path: '/view', component: InboxPage },
+        { path: '/inbox', alias: '/view?page=inbox' },
+        { path: '/important', alias: '/view?page=important' },
+        { path: '/starred', alias: '/view?page=starred' },
+        { path: '/sent', alias: '/view?page=sent' },
+        { path: '/trash', alias: '/view?page=trash' },
+    ],
 });
 
 export default router;
